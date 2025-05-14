41f5f9d6828eac085645a0d9898444380b3dfea0
Arhan <arhandonerkaya20@gmail.com>
2025-05-15 01:12:43 +0300

    ALLOWED_HOSTS: domainler eklendi

--- arhanceviri/settings.py
+++ arhanceviri/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-9&k#80g71i%8wg1++=ar%nt!&zzvocwrtm_*1v6rm&id%2eg&&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['codey-translate.onrender.com']
+ALLOWED_HOSTS = [
+    'codey-translate.onrender.com',
+    'codeytranslate.xyz',
+    'www.codeytranslate.xyz'
+    ]
 
 
 # Application definition
